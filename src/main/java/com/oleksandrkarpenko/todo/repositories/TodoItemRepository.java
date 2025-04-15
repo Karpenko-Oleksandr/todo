c2ffc94fb7fa48c572f52b3a89a83edbc30117c0
Oleksandr Karpenko <1ooksapp8@gmail.com>
2025-04-15 10:07:08 +0300

    Created model for Todo items and directory repositories with interface TodoItemRepository

--- src/main/java/com/oleksandrkarpenko/todo/repositories/TodoItemRepository.java
+++ src/main/java/com/oleksandrkarpenko/todo/repositories/TodoItemRepository.java
@@ -3,5 +3,4 @@ package com.oleksandrkarpenko.todo.repositories;
 import com.oleksandrkarpenko.todo.model.TodoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
-}
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {}
