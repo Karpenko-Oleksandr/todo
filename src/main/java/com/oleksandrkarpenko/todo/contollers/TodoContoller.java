fdaaf38f256e185eafc047ae77d19a7c66cba446
Karpenko Oleksandr <161121808+Karpenko-Oleksandr@users.noreply.github.com>
2025-04-18 09:29:18 +0300

    SearchItems
    * Added Delete button for items
    
    * Added the button Remove all items from the to do list
    
    * Added the button Remove all items from the to do list
    
    * Added the button Remove all items from the to do list
    
    * Added the button Remove all items from the to do list
    
    * Implemented search for items in the to-do list

--- src/main/java/com/oleksandrkarpenko/todo/contollers/TodoContoller.java
+++ src/main/java/com/oleksandrkarpenko/todo/contollers/TodoContoller.java
@@ -6,11 +6,15 @@ import com.oleksandrkarpenko.todo.repositories.TodoItemRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -43,6 +47,30 @@ public class TodoContoller implements CommandLineRunner {
     public String deleteTodoItem(@PathVariable("id") Long id) {
         todoItemRepository.deleteById(id);
         return "redirect:/";
+    }
+
+    @PostMapping("/removeAll")
+    public String removeAllTodoItems() {
+        todoItemRepository.deleteAll();
+        return "redirect:/";
+    }
+
+    @PostMapping("/search")
+    public String searchTodoItems(@RequestParam("searchTerm") String searchTerm, Model model) {
+        List<TodoItem> allItems = todoItemRepository.findAll();
+        List<TodoItem> searchResults = new ArrayList<>();
+
+        for (TodoItem item : allItems) {
+            if (item.getTitle().toLowerCase().contains(searchTerm.toLowerCase())) {
+                searchResults.add(item);
+            }
+        }
+
+        model.addAttribute("allTodos", searchResults);
+        model.addAttribute("newTodo", new TodoItem());
+        model.addAttribute("searchTerm", searchTerm);
+
+        return "index";
     }
 
     @Override
